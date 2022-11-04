fa3ba1b190b59b07b73419a6e12c5fe23ad642e8
Qin Guan <qinguan20040914@gmail.com>
2022-11-05 00:56:39 +0800

    chore: test

---
+++ examples/nuxt-daisyui/pages/index.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+const state = ref("")
+</script>
+
+<template>
+  <div>
+    index
+    {{ state }}
+    <input v-model="state" />
+    <NuxtLink to="/second">second</NuxtLink>
+  </div>
+</template>
